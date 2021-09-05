466bd1f10c62959f4a0ce36bc80f9479933c99b8
shruthishruu05 <shruthi.m@perfios.com>
2021-09-05 21:59:39 +0530

    ADDED : class that takes input from console

---
+++ src/com/bridgelabz/commercialdataprocessing/StockAccount.java
@@ -0,0 +1,157 @@
+package com.bridgelabz.commercialdataprocessing;
+
+import java.util.Scanner;
+
+import com.bridgelabz.linkedlist.LinkedList;
+import com.bridgelabz.linkedlist.MyNode;
+import com.bridgelabz.linkedlist.MyQueue;
+import com.bridgelabz.linkedlist.MyStack;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;  
+
+public class StockAccount {
+	LinkedList<CompanyShare> myShareList;
+	MyStack<String> purchasedStack = new MyStack<String>();
+	MyStack<String> soldStack = new MyStack<String>();
+	MyQueue<String> transactionQueue = new MyQueue<String>();
+	Double total ;
+	DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
+	public StockAccount() {
+		myShareList =new LinkedList<CompanyShare>();
+		total = 0.0;
+	}
+	public void addCompanyShares() {
+		System.out.println("Enter the number of CompanyShares");
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		for(int i=0;i<count;i++) {
+			System.out.println("enter the Stock Symbol");
+			String symble = sc.next();
+			System.out.println("Enter the number of shares");
+			int numOfShares = sc.nextInt();
+			System.out.println("Enter the price of one share");
+			double price = sc.nextDouble();
+			CompanyShare stock = new CompanyShare(symble,numOfShares,price);
+			MyNode<CompanyShare> myStockNode = new MyNode<CompanyShare>(stock);
+			myShareList.append(myStockNode);
+			
+		}
+	}
+	
+	public void removeCompanyShares(String symbol) {
+		Scanner sc = new Scanner(System.in);
+		MyNode<CompanyShare> tempNode = (MyNode<CompanyShare>) myShareList.head;
+		while(tempNode != null) {
+			String currentSymbol = tempNode.getKey().getStockSymbol();
+			if(currentSymbol.equals(symbol)) {
+				myShareList.delete(tempNode.getKey());
+				System.out.println("deleted Company share with symbol : "+tempNode.getKey().getStockSymbol());
+				return;
+			}
+			tempNode = (MyNode<CompanyShare>)tempNode.getNext();
+		}
+		System.out.println("CompanyShare with symbol "+symbol+" is not found");
+	}
+	public double valueOf() {
+		this.total = 0.0;
+		MyNode<CompanyShare> tempNode = (MyNode<CompanyShare>) myShareList.head;
+		while(tempNode != null) {
+			this.total += tempNode.getKey().getValue();
+			tempNode = (MyNode<CompanyShare>)tempNode.getNext();
+		}
+		return this.total;
+	}
+	
+	public void buy(int amount, String symbol) {
+		MyNode<CompanyShare> tempNode = (MyNode<CompanyShare>) myShareList.head;
+		while(tempNode != null) {
+			if(tempNode.getKey().getStockSymbol().equals(symbol)) {
+				int currentShares = tempNode.getKey().getNumberOfShare();
+				tempNode.getKey().setNumberOfShare(amount+currentShares);
+				double value = tempNode.getKey().getPricePerShare() * (amount+currentShares);
+				this.total = value;
+				tempNode.getKey().setValue(value);
+				purchasedStack.push(symbol);
+				System.out.println(" added "+amount+" shares to stockSymbol "+symbol+" updated value is "+value);
+				tempNode.getKey().setDateTime(LocalDateTime.now());
+				transactionQueue.enqueue(LocalDateTime.now().format(format).toString());
+				return;
+			}
+			tempNode = (MyNode<CompanyShare>)tempNode.getNext();
+		}
+		System.out.println("Symbol not found");
+	}
+	
+	public void sell(int amount, String symbol) {
+		MyNode<CompanyShare> tempNode = (MyNode<CompanyShare>) myShareList.head;
+		while(tempNode != null) {
+			if(tempNode.getKey().getStockSymbol().equals(symbol)) {
+				int currentShares = tempNode.getKey().getNumberOfShare();
+				if(currentShares-amount < 0) {
+					System.out.println("There is not enough shares to sell. pls reduce amount!");
+					return;
+				}
+				tempNode.getKey().setNumberOfShare(currentShares-amount);
+				double value = tempNode.getKey().getPricePerShare() * (currentShares-amount);
+				tempNode.getKey().setValue(value);
+				this.total = value;
+				soldStack.push(symbol);
+				System.out.println(" sold "+amount+" shares of stockSymbol "+symbol+" updated value is "+value);
+				tempNode.getKey().setDateTime(LocalDateTime.now());
+				transactionQueue.enqueue(LocalDateTime.now().format(format).toString());
+				return;
+			}
+			tempNode = (MyNode<CompanyShare>)tempNode.getNext();
+		}
+		System.out.println("Symbol not found");
+	}
+	
+	public void printReport() {
+		System.out.println("**** The stock report ****");
+		System.out.println("The total value is : "+ this.total);
+		myShareList.display();
+	}
+	
+	public void printStacks() {
+		System.out.println("The purchased stocks are (Latest first) ");
+		MyStack<String> buffer = new MyStack<String>();
+		while(!purchasedStack.isEmpty()) {
+			String str = purchasedStack.pop();
+			buffer.push(str);
+			System.out.print(str+" ");
+		}
+		purchasedStack = new MyStack<String>();
+		while(!buffer.isEmpty()) {
+			purchasedStack.push(buffer.pop());
+		}
+		System.out.println();
+		buffer = new MyStack<String>();
+		System.out.println("The sold stocks are (Latest first) ");
+		while(!soldStack.isEmpty()) {
+			String str = soldStack.pop();
+			buffer.push(str);
+			System.out.print(str+" ");
+		}
+		soldStack = new MyStack<String>();
+		while(!buffer.isEmpty()) {
+			soldStack.push(buffer.pop());
+		}
+		System.out.println();
+	}
+	
+	public void printTimeLineQueue() {
+		System.out.println(" transactions processed till now");
+		MyQueue<String> buffer = new MyQueue<String>();
+		while(!transactionQueue.isEmpty()) {
+			String str = transactionQueue.dequeue();
+			buffer.enqueue(str);
+			System.out.print(str + " -- ");
+		}
+		transactionQueue = new MyQueue<String>();
+		while(!buffer.isEmpty()) {
+			transactionQueue.enqueue(buffer.dequeue());
+		}
+		System.out.println();
+	}
+}
---
+++ src/com/bridgelabz/commercialdataprocessing/StockAccountMain.java
@@ -0,0 +1,65 @@
+package com.bridgelabz.commercialdataprocessing;
+
+import java.util.Scanner;
+
+public class StockAccountMain {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Stock account management");
+		StockAccount myStocks = new StockAccount();
+		myStocks.addCompanyShares();
+		myStocks.valueOf();
+		boolean flag = true;
+		while(flag) {
+			System.out.println("Select 1:add  2: remove 3:buy  4:sell 5: get Report  6:get value  7:purchased-sold stocks  8:Stock_Timeline   9:exit");
+			int option = sc.nextInt();
+			switch(option) {
+				case 1:
+					myStocks.addCompanyShares();
+					myStocks.valueOf();
+					break;
+				case 2:
+					System.out.println("Enter the Share Symbol is remove it");
+					myStocks.removeCompanyShares(sc.next());
+					myStocks.valueOf();
+					break;
+				case 3:
+					System.out.println("enter the amount of share");
+					int amount = sc.nextInt();
+					System.out.println("enter the stock symbol	");
+					String symbol = sc.next();
+					myStocks.buy(amount, symbol);
+					myStocks.valueOf();
+					break;
+				case 4:
+					System.out.println("enter the amount of share");
+					int amount1 = sc.nextInt();
+					System.out.println("enter the stock symbol");
+					String symbol1 = sc.next();
+					myStocks.sell(amount1, symbol1);
+					myStocks.valueOf();
+					break;
+				case 5:
+					myStocks.printReport();
+					break;
+				case 6:
+					System.out.println("the total value of shares : " +myStocks.valueOf());
+					break;
+				case 7:
+					myStocks.printStacks();
+					break;
+				case 8:
+					myStocks.printTimeLineQueue();
+					break;
+				default:
+					flag = false;
+					break;	
+			}
+		}
+			
+		sc.close();
+
+	}
+
+}
